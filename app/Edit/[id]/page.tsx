cb86a76a7c9167052c1236e9dfc43ea5dd133534
AnimeshA867 <animeshacharya867+github@gmail.com>
2024-01-04 13:25:44 +0545

    Changed few things

--- app/Edit/[id]/page.tsx
+++ app/Edit/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const getData = async (id) => {
   try {
-    const res = await fetch(`http://localhost:3000/api/data/${id}`, {
+    const res = await fetch(`${process.env.PUBLIC_URL}/api/data/${id}`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     });
